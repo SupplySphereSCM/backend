8eb6c9b57dd04fd65c14631a659709d8a9919693
nishanth-code <nishanthnavneeth200@gmail.com>
2024-04-15 14:00:04 +0530

    ADD: defined order and iovoice enteties

--- src/modules/invoice/entities/invoice.entity.ts
+++ src/modules/invoice/entities/invoice.entity.ts
@@ -1,1 +1,47 @@
-export class Invoice {}
+
+import { User } from "src/modules/users/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export class ProductList{
+    @Column({})
+    product:string;
+
+    @Column({})
+    quantity:number;
+
+    @Column({})
+    price:number;
+
+    @Column({})
+    subaTotal:number;
+}
+@Entity({
+    name:Invoice.name
+})
+export class Invoice {
+    @PrimaryGeneratedColumn({})
+    id: string;
+
+    @ManyToOne(()=> User,(user) => user)
+    from:User;
+
+    @ManyToOne(()=> User,(user) => user)
+    to:User;
+
+    @Column({type:'jsonb'})
+    products:ProductList[];
+
+    @Column({})
+    cgst:number;
+
+    @Column({})
+    sgst:number;
+
+    @Column({})
+    total:number;
+
+    
+
+
+
+}
--- src/modules/orders/entities/order.entity.ts
+++ src/modules/orders/entities/order.entity.ts
@@ -1,1 +1,32 @@
-export class Order {}
+import { ProductList } from "src/modules/invoice/entities/invoice.entity";
+import { User } from "src/modules/users/entities/user.entity";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+export enum STATUS {
+    ORDERED = 'ORDERED',
+    TRANSIT = 'TRANSIT',
+    DELIVERED = 'DELIVERED'
+  }
+export class Order {
+    @PrimaryGeneratedColumn({})
+    id: string;
+
+    @Column({
+        type: 'enum',
+        enum: STATUS,
+        default: STATUS.ORDERED,
+      })
+    orderStatus:STATUS;
+
+    @ManyToOne(()=> User,(user) => user)
+    from:User;
+
+    @ManyToOne(()=> User,(user) => user)
+    to:User;
+
+    @Column({type:'jsonb'})
+    products:ProductList[];
+
+    @Column({})
+    total:number;
+}
