64ccca317493395dde87816f23b4f3d456e40d48
nishanth-code <nishanthnavneeth200@gmail.com>
2024-05-13 21:15:06 +0530

    UPDATE: order entity updating

--- src/modules/orders/dto/update-order.dto.ts
+++ src/modules/orders/dto/update-order.dto.ts
@@ -1,4 +1,30 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
+import { IsEnum, IsNumber, IsOptional } from 'class-validator';
+import { STATUS } from '../entities/order.entity';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto  {
+    @IsOptional()
+    @IsNumber()
+    total:number;
+
+    @IsOptional()
+    @IsNumber()
+    quantity:number;
+
+    @IsOptional()
+    @IsNumber()
+    tax:number;
+
+    @IsOptional()
+    @IsNumber()
+    deliveryCharges:number;
+
+    @IsOptional()
+    @IsEnum(STATUS)
+    orderStatus:STATUS;
+
+    @IsOptional()
+    @IsEnum(STATUS)
+    stepType:STATUS;
+}
